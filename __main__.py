06e891fb002a93ada6af91fe413944036fb9e6c0
Anish Naskar <anishnaskar.work@gmail.com>
2025-07-15 19:31:51 +0530

    Added running command
    right now using simply subprocess to run simple command, Later going to replace with Intent recognition and other stuff

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ from config.config_manager import ConfigManager
 from core.audio_input_pipeline import detect_voice, transcribe_audio
 from core.template_generator import BiometricTemplateGenerator
 import stubs.wake_up_detection as wud
-import time
+from core.assistant import run_command
 
 def main():
     config_mgr = ConfigManager()
@@ -12,10 +12,16 @@ def main():
         print('==================================================================')
         voice = detect_voice(config=config_mgr)
 
+        transcript = transcribe_audio(config=config_mgr, audio=voice)
         if wud.wake_up_detection_stub(ip=transcript) and biometric_template.match_embedding(voice):
         # if biometric_template.match_embedding(voice):
+            
+            print("Listening for command...")
+            voice = detect_voice(config=config_mgr)
             transcript = transcribe_audio(config=config_mgr, audio=voice)
-            print(transcript)
+            print(f"Command: {transcript}")
+            run_command(transcript)
+
         else:
             print("Failed Auth or Failed Wake Up")
             continue
--- __test__.py
+++ __test__.py
@@ -1,19 +1,4 @@
-from rapidfuzz import fuzz
-from core.audio_input_pipeline import transcribe_audio, detect_voice
-from config.config_manager import ConfigManager
-from core.template_generator import BiometricTemplateGenerator
-import numpy as np
-import stubs.wake_up_detection as wad
-from resemblyzer import preprocess_wav
-
-config_mgr = ConfigManager()
-
-def template_gen():
-    biometric_template = BiometricTemplateGenerator(config_mgr=config_mgr)
-    audio = preprocess_wav("./samples/wavf001_2025-07-07.wav")
-
-    match = biometric_template.match_embedding(audio=audio)
-    print(match)
+from core.assistant import run_command
 
 
-template_gen()
+run_command("open spotify")
